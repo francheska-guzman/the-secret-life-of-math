346cb30f674bf719a6d3dcdcd1730a08444a7474
Francheska <gffrancheska@outlook.com>
2017-03-11 00:32:52 -0500

    Updating

--- myScript.js
+++ myScript.js
@@ -1,3 +1,10 @@
-function average() {
+var myNumbers = document.getElementById("myNum");
+var myArray = myNumbers.split(" ");
+var sum;
 
+function average() {
+for (var i = 0; i < myArray.length; i++) {
+	sum = sum + myArray[i];
+	}
+	document.getElementById("output").innerHTML = (sum/myArray.length);
 };
